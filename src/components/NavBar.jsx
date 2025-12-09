17036c00f897fd1d324f6bc3749573c605fe9df1
tomiswe05 <oyaniyie@gmail.com>
2025-12-09 03:16:31 +0000

    Trigger rebuild: trivial change in NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import "./NavBar.css";
 import arrow from '../assets/images/arrow.svg';
 
 const Navbar = () => {
